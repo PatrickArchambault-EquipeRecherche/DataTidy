7612fc444e9251355ecd1b57293b93f82b9d1079
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2021-05-06 11:12:43 -0400

    More basic formatting, comments, trying an empy file to empty file converstion

--- DataTidy.py
+++ DataTidy.py
@@ -21,11 +21,38 @@ import datetime
 import csv
 import pandas
 
-# Start by pulling in the Parameter file.  By default this will be 
-# 'parameters.csv', but should eventually be specifiable on the command 
-# line.
+# Following are the base validation functions
 
 def dateCheck(myDateString):
     # Parse the date using the datetime tools, the put the date into the
     # format needed for consistency, adding an Error Value if the parsing
     # fails and a default value if there is nothing in the cell.
+    pass
+
+# Start by pulling in the Parameter file.  By default this will be 
+# 'parameters.csv', but should eventually be specifiable on the command 
+# line.
+
+with open('parameters.csv', newline='') as parametersfile:
+    parameterReader =  pandas.read_csv(parametersfile)
+
+    # Next open the source datafile.  By default this will be 'source.csv' 
+    # but should be specifiable on the command line.
+
+    with open('source.csv', newline='') as sourcefile:
+        sourceReader = csv.reader(sourcefile)
+
+        # Finally, open the destination file.  Having all of these files 
+        # open at once ensures that we break on missing or misnamed files 
+        # right away, and lets use read and write only one file at a 
+        # time, keeping the memory needs very low and the number of times 
+        # we loop through the source file to one.
+
+        # Use datetime.now() to give the output file a name unique to a 
+        # given minute.
+        myDateTimeString = datetime.datetime.now().strftime("%Y%m%d%H%M")
+
+        with open(myDateTimeString + "tidyData.csv" , newline='') as outputfile:
+            for row in sourceReader:
+                outputfile.writerow(row)
+
